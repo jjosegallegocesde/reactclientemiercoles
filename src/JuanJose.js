591d3cfe567a9498e53148d762552bc0ae1e905e
Albeiro Muriel <j.albeiromuriel@gmail.com>
2022-06-01 09:24:49 -0500

    se crea cliente

---
+++ src/JuanJose.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react"
+
+export function JuanJose(){
+
+    let universidad='CESDE'
+    let suma=5+1
+
+    return(
+        <>
+            <h1>Hola soy Juan Jose</h1>
+            {/*<h4>Y su tu amigo....</h4>*/}
+            <h5>Y estudio en {universidad}</h5>
+            <h6>la suma de 5+1 es {5+1}</h6>
+            
+        </>
+       
+        
+    )
+}
